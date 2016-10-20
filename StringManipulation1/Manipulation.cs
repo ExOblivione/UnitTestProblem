db840f1d4e0c09528ffcc131a1ce4448b1be25e9
ExO <ExO@DEVENV>
2016-10-20 20:06:28 +0000

    Add project files.

---
+++ StringManipulation1/Manipulation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace StringManipulation1
+{
+    class Manipulation
+    {
+        static void Main(string[] args)
+        {
+            // I write out a test data
+            Console.WriteLine(StringShift("Microsoft<<2"));
+        }
+
+        static string StringShift(string input)
+        {
+            // variables for getting the operator indexes by trimming the input string
+            // operatorr will store the found operator, before the trimming it's an empty string
+            int indexOfOperator = 0;
+            int operatorParameter = 0;
+            string operatorr = "";
+
+            // looking for an exact operator
+            if (input.Contains("<<"))
+            {
+                // store the index of an integer which shows how many strings I want to trim
+                indexOfOperator = input.IndexOf("<<") + 2;
+                operatorr = "<<";
+            }
+            if (input.Contains(">>"))
+            {
+                indexOfOperator = input.IndexOf(">>") + 2;
+                operatorr = ">>";
+            }
+
+            // store the integer which shows how many strings I want to trim
+            operatorParameter = int.Parse(input.Substring(indexOfOperator, input.Length - indexOfOperator).Trim());
+
+            // now we have both parameters to work on the string
+            string purestring = input.Substring(0, indexOfOperator - 2);
+            switch (operatorr)
+            {
+                //specify which shift does what job
+                case ">>":
+                    // put the trimmed string from the beginning of the pure string to the end of the pure string
+                    purestring = purestring.Substring(operatorParameter, purestring.Length - operatorParameter)
+                        + purestring.Substring(0, operatorParameter);
+                    break;
+                case "<<":
+                    // put the trimmed string from the end of the pure string to the beginning of the pure string
+                    purestring = purestring.Substring(purestring.Length - operatorParameter, operatorParameter)
+                        + purestring.Substring(0, purestring.Length - operatorParameter);
+                    break;
+            }
+
+            // give back the updated string to main()
+            return purestring;
+        }
+    }
+}
---
+++ StringManipulationUnitTest/Tester.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringManipulationUnitTest
+{
+    [TestClass]
+    public class SMUTest
+    {
+        Stringmanipulation1 testing = new Stringmanipulation1();
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // This should fail, because the number is bigger than the length of the string
+            string expectedResult = "testword";
+            string actualString = "testword>>11";
+            
+            string actualResult = testing.StringShift(actualString);
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // This should pass, and would give this output: cereferen
+            string expectedResult = "cereferen";
+            string actualString = "reference<<2";
+
+            string actualResult = testing.StringShift(actualString);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            // This should pass, and would give this output: alyzean
+            string expectedResult = "alyzean";
+            string actualString = "analyze>>2";
+
+            string actualResult = testing.StringShift(actualString);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            // This should pass, and would give this output: ionsolu
+            string expectedResult = "ionsolu";
+            string actualString = "solution<<3";
+
+            string actualResult = testing.StringShift(actualString);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            // This should pass, and would give this output: rtiesprope
+            string expectedResult = "rtiesprope";
+            string actualString = "properties>>5";
+
+            string actualResult = testing.StringShift(actualString);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            // This should pass, and would give this output: urationconfig
+            string expectedResult = "urationconfig";
+            string actualString = "configuration<<7";
+
+            string actualResult = testing.StringShift(actualString);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
